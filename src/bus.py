585eb6e8f73b03281e88d392d79ba7ba838f3ab3
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-10-20 14:30:25 +0100

    first commit for first test

---
+++ src/bus.py
@@ -0,0 +1,5 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers  = []
---
+++ src/bus_stop.py
@@ -0,0 +1,3 @@
+class BusStop:
+    def __init__ (self, name):
+        self.name = name    
